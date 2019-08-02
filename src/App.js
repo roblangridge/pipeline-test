5b6fab0693f1c80d70ad0271011f7ae5f37e56bc
Rob Langridge <rob@belimitless.io>
2019-08-02 20:31:38 +0100

    Very important change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React :) hooray!
+          Learn React :) hooray!!
         </a>
       </header>
       <Hat />
