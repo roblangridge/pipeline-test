814802e8a5f029620c8c6ce260d6566af7b52cd9
Rob Langridge <langrir@gmail.com>
2019-08-22 13:21:27 +0100

    Name change

--- src/TitleUtil.test.js
+++ src/TitleUtil.test.js
@@ -3,5 +3,5 @@ import { getTitle } from "./TitleUtil";
 it("should return a title", () => {
     const title = getTitle();
 
-    expect(title).toEqual("Learn React");
+    expect(title).toEqual("Learn React!");
 });
